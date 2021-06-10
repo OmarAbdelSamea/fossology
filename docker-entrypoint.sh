df586be158012235d609dc18e60aedc7f259441f
OmarAbdelSamea <1700903@eng.asu.edu.eg>
2021-06-10 13:50:40 +0200

    Entry point configured

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,40 +12,47 @@
 
 set -o errexit -o nounset -o pipefail
 
-db_host="${FOSSOLOGY_DB_HOST:-localhost}"
+db_host="${FOSSOLOGY_DB_HOST:-db}"
 db_name="${FOSSOLOGY_DB_NAME:-fossology}"
 db_user="${FOSSOLOGY_DB_USER:-fossy}"
 db_password="${FOSSOLOGY_DB_PASSWORD:-fossy}"
 
 # Write configuration
-cat <<EOM > /usr/local/etc/fossology/Db.conf
+cat <<EOM > /etc/fossology/Db.conf
 dbname=$db_name;
 host=$db_host;
 user=$db_user;
 password=$db_password;
 EOM
 
-sed -i 's/address = .*/address = '"${FOSSOLOGY_SCHEDULER_HOST:-localhost}"'/' \
-    /usr/share/etc/fossology/fossology.conf
+sed -i 's/address = .*/address = '"${FOSSOLOGY_SCHEDULER_HOST:-db}"'/' \
+    /etc/fossology/fossology.conf
 
 # Startup DB if needed or wait for external DB
-if [[ $db_host == 'localhost' ]]; then
-  echo '*****************************************************'
-  echo 'WARNING: No database host was set and therefore the'
-  echo 'internal database without persistency will be used.'
-  echo 'THIS IS NOT RECOMENDED FOR PRODUCTIVE USE!'
-  echo '*****************************************************'
-  sleep 5
-  /etc/init.d/postgresql start
-else
-  test_for_postgres() {
-    PGPASSWORD=$db_password psql -h "$db_host" "$db_name" "$db_user" -c '\l' >/dev/null
-    return $?
-  }
-  until test_for_postgres; do
-    >&2 echo "Postgres is unavailable - sleeping"
-    sleep 1
-  done
+if [[ "$1" == "scheduler" ]]; then
+  if [[ $db_host == 'db' ]]; then
+    echo '*****************************************************'
+    echo 'WARNING: No database host was set and therefore the'
+    echo 'internal database without persistency will be used.'
+    echo 'THIS IS NOT RECOMENDED FOR PRODUCTIVE USE!'
+    echo '*****************************************************'
+    sleep 5
+    /etc/init.d/postgresql start
+  else
+    test_for_postgres() {
+      PGPASSWORD=$db_password psql -h "$db_host" "$db_name" "$db_user" -c '\l' >/dev/null
+      return $?
+    }
+    until test_for_postgres; do
+      >&2 echo "Postgres is unavailable - sleeping"
+      sleep 1
+    done
+  fi
+fi  
+
+# # Setup environment
+if [[ $# -eq 0 || ($# -eq 1 && "$1" == "scheduler") ]]; then
+    /usr/lib/fossology/fo-postinstall --common --database --licenseref
 fi
 
 # Start Fossology
@@ -54,19 +61,21 @@ echo 'Fossology initialisation complete; Starting up...'
 echo
 if [[ $# -eq 0 ]]; then
   /etc/init.d/cron start
-  /usr/share/fossology/scheduler/agent/fo_scheduler \
+  /etc/fossology/mods-enabled/scheduler/agent/fo_scheduler \
     --log /dev/stdout \
     --verbose=3 \
     --reset &
   /usr/sbin/apache2ctl -D FOREGROUND
 elif [[ $# -eq 1 && "$1" == "scheduler" ]]; then
-  exec /usr/share/fossology/scheduler/agent/fo_scheduler \
+  exec /etc/fossology/mods-enabled/scheduler/agent/fo_scheduler \
     --log /dev/stdout \
     --verbose=3 \
     --reset
 elif [[ $# -eq 1 && "$1" == "web" ]]; then
-  /etc/init.d/cron start
+  service cron start
   exec /usr/sbin/apache2ctl -e info -D FOREGROUND
+elif [[ $# -eq 1 && "$1" == "agent" ]]; then
+  echo "agent"
 else
   exec "$@"
 fi
